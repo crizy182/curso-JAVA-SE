0f21a6df4094b70563e8c88b7265a2878282df65
cristopher <cristhiam.tovar@mail.escuelaing.edu.co>
2019-05-08 09:36:01 -0500

    Encapsulando atributos

--- src/com/anncode/amazonviewer/Main.java
+++ src/com/anncode/amazonviewer/Main.java
@@ -7,15 +7,17 @@ import com.anncode.amazonviewer.model.Movie;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		showMenu();
+
+		Movie movie = new Movie("Coco", "Animation", (short) 2017);
+		movie.showData();
+		// showMenu();
 
 	}
-	
+
 	public static void showMenu() {
 		int exit = 0;
 		do {
-			
+
 			System.out.println("BIENVENIDOS AMAZON VIEWER");
 			System.out.println("");
 			System.out.println("Selecciona el número de la opción deseada");
@@ -26,103 +28,99 @@ public class Main {
 			System.out.println("5. Report");
 			System.out.println("6. Report Today");
 			System.out.println("0. Exit");
-			
-			//Leer la respuesta del usuario
+
+			// Leer la respuesta del usuario
 			int response = 1;
 			switch (response) {
-				case 0:
-					//salir
-					
-					break;
-				case 1:
-					showMovies();
-					break;
-				case 2:
-					showSeries();
-					break;
-				case 3:
-					showBooks();
-					break;
-				case 4:
-					showMagazines();
-					break;
-				case 5:
-					makeReport();
-					break;
-				case 6:
-					//Date date = new Date();
-					makeReport(new Date());
-					break;
-	
-				default:
-					System.out.println();
-					System.out.println("....¡¡Selecciona una opción!!....");
-					System.out.println();
-					break;
+			case 0:
+				// salir
+
+				break;
+			case 1:
+				showMovies();
+				break;
+			case 2:
+				showSeries();
+				break;
+			case 3:
+				showBooks();
+				break;
+			case 4:
+				showMagazines();
+				break;
+			case 5:
+				makeReport();
+				break;
+			case 6:
+				// Date date = new Date();
+				makeReport(new Date());
+				break;
+
+			default:
+				System.out.println();
+				System.out.println("....¡¡Selecciona una opción!!....");
+				System.out.println();
+				break;
 			}
-			
-			
-		}while(exit != 0);
+
+		} while (exit != 0);
 	}
-	
+
 	public static void showMovies() {
 		int exit = 0;
 		do {
 			System.out.println();
 			System.out.println(":: MOVIES ::");
 			System.out.println();
-		}while(exit !=0);
-		
+		} while (exit != 0);
+
 	}
-	
+
 	public static void showSeries() {
 		int exit = 0;
 		do {
 			System.out.println();
 			System.out.println(":: SERIES ::");
 			System.out.println();
-		}while(exit !=0);
+		} while (exit != 0);
 	}
-	
+
 	public static void showChapters() {
 		int exit = 0;
 		do {
 			System.out.println();
 			System.out.println(":: CHAPTERS ::");
 			System.out.println();
-		}while(exit !=0);
+		} while (exit != 0);
 	}
-	
+
 	public static void showBooks() {
 		int exit = 0;
 		do {
 			System.out.println();
 			System.out.println(":: BOOKS ::");
 			System.out.println();
-		}while(exit !=0);
+		} while (exit != 0);
 	}
-	
+
 	public static void showMagazines() {
 		int exit = 0;
 		do {
 			System.out.println();
 			System.out.println(":: MAGAZINES ::");
 			System.out.println();
-		}while(exit !=0);
+		} while (exit != 0);
 	}
-	
+
 	public static void makeReport() {
-		
+
 	}
-	
+
+	// El siguiente metodo es el mismo que el anterior pero se encuentra
+	// sobrecargado con el argumento
+	// date de la clase Date su valor es ingresado en la línea 54
 	public static void makeReport(Date date) {
-		
+
 	}
-	
+
 }
-
-
-
-
-
-
